181c1005dc2814d5fc6d5ecb0430c80c309dd016
obedmacallums <obedmacallums@gmail.com>
2021-05-17 22:27:01 -0400

    added example login

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,7 +3,7 @@
     
     <div class="text-gray-400">
         <router-link to="/">
-        <div class="h-8 w-8  hover:text-white hover:border-white">
+        <div class="h-10 w-10  hover:text-white hover:border-white">
              
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="w-full h-full" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M2 13.5V7h1v6.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V7h1v6.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5zm11-11V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
@@ -45,20 +45,31 @@
 </div>
 
 <div @click="menu_status()">
-  
-        <button id='boton' class="flex items-center px-2 py-1 text-gray-400 border border-gray-600 rounded hover:text-white hover:border-white">
+        <div class="h-12 w-12">
+        <div id='boton' class="text-gray-400 border py-1 px-2 border-gray-600 rounded hover:text-white hover:border-white flex items-center">
       
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-justify" viewBox="0 0 16 16">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="w-full h-full" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M2 12.5a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z"/>
               </svg>
-          </button>
+        </div>
+        </div>
   
     </div>
 
   
   
     <div id='menu' class="flex-grow  w-full hidden" >
-      <div class="">
+      
+
+                <a href="#" class="block mt-3 font-sans text-gray-400 text-md hover:text-white">
+            
+             <i><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="inline" viewBox="0 0 16 16">
+  <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+  <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm12 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1v-1c0-1-1-4-6-4s-6 3-6 4v1a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12z"/>
+</svg></i>&nbsp;
+              obedmacallums@gmail.com
+              
+          </a>  
         
         <router-link to="/login" class="block mt-3 font-sans text-gray-400 text-md hover:text-white">
           <i><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="inline" viewBox="0 0 16 16">
@@ -74,7 +85,7 @@
               </svg></i>&nbsp;
           Este mes 
         </a>
-        <div class="flex justify-between">
+        
         <a href="#" class="block mt-3 font-sans text-gray-400 text-md hover:text-white">
             <i><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="inline" viewBox="0 0 16 16">
                 <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
@@ -82,19 +93,13 @@
               </svg></i>&nbsp;
           Cerrar Sesion
         </a>
-        <a href="#" class="block mt-3 font-sans text-gray-400 text-md hover:text-white">
-            obedmacallums@gmail.com
-             <i><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="inline" viewBox="0 0 16 16">
-                <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
-              </svg></i>
-              
-          </a>   
-        </div>
+ 
+        
         
   
   
        
-      </div>
+      
       
     </div>
     
--- src/views/Add.vue
+++ src/views/Add.vue
@@ -1,6 +1,46 @@
 <template>
     <div>
-        Add
+        <div>
+        <div class="h-20 m-4 flex flex-col">
+        <label for="des" class="font-sans text-lg font-bold">Descripcion</label>
+        
+        <input id="des" type="text" class="w-full h-full shadow-md rounded-lg border border-gray-400 focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent">
+        </div>
+
+        <div class="h-20 m-4 flex flex-col">
+            <label for="item" class="font-sans text-lg font-bold">Item</label>
+            
+            <select id="item" class="w-full h-full shadow-md rounded-lg border border-gray-400 focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent">
+            <option value="volvo">Volvo</option>
+            <option value="saab">Saab</option>
+            <option value="mercedes">Mercedes</option>
+            <option value="audi">Audi</option>
+            </select>
+            </div>
+
+        <div class="h-20 m-4 flex flex-col">
+            <label for="amount" class="font-sans text-lg font-bold">Monto</label>
+            
+            <input id="amount" type="number" class="w-full h-full shadow-md rounded-lg border border-gray-400 focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent">
+            </div>
+
+        <div class="h-20 m-4 flex flex-col">
+            <label for="date" class="font-sans text-lg font-bold">Fecha</label>
+            
+            <input id="date" class="w-full h-full shadow-md rounded-lg border border-gray-400 focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent">
+            </div>
+
+        </div>
+
+        <div class="flex place-content-between m-4">
+         <button class="font-semibold font-sans py-2 px-6 rounded-xl bg-gray-700 text-white hover:bg-gray-900">
+            Borrar
+        </button>
+
+        <button class="font-semibold font-sans py-2 px-6 rounded-xl bg-gray-700 text-white hover:bg-gray-900">
+            Guardar
+        </button>   
+        </div>
     </div>
 </template>
 
