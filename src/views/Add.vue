8ff1d0d7d679861ac26a1c6f38b9cdb7a00f7263
obedmacallums <obedmacallums@gmail.com>
2021-05-23 14:45:06 -0400

    added datepicker

--- src/views/Add.vue
+++ src/views/Add.vue
@@ -1,16 +1,16 @@
 <template>
     <div>
         <div>
-        <div class="h-20 m-4 flex flex-col">
+        <div class="flex flex-col h-20 m-4">
         <label for="des" class="font-sans text-lg font-bold">Descripcion</label>
         
-        <input id="des" type="text" class="w-full h-full shadow-md rounded-lg border border-gray-400 focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent">
+        <input id="des" type="text" class="w-full h-full border border-gray-400 rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent">
         </div>
 
-        <div class="h-20 m-4 flex flex-col">
+        <div class="flex flex-col h-20 m-4">
             <label for="item" class="font-sans text-lg font-bold">Item</label>
             
-            <select id="item" class="w-full h-full shadow-md rounded-lg border border-gray-400 focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent">
+            <select id="item" class="w-full h-full border border-gray-400 rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent">
             <option value="volvo">Volvo</option>
             <option value="saab">Saab</option>
             <option value="mercedes">Mercedes</option>
@@ -18,26 +18,27 @@
             </select>
             </div>
 
-        <div class="h-20 m-4 flex flex-col">
+        <div class="flex flex-col h-20 m-4">
             <label for="amount" class="font-sans text-lg font-bold">Monto</label>
             
-            <input id="amount" type="number" class="w-full h-full shadow-md rounded-lg border border-gray-400 focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent">
+            <input id="amount" type="number" class="w-full h-full border border-gray-400 rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent">
             </div>
 
-        <div class="h-20 m-4 flex flex-col">
+        <div class="flex flex-col h-20 m-4">
             <label for="date" class="font-sans text-lg font-bold">Fecha</label>
+            <datepicker v-model="picked" :inputFormat="format" :locale="locale" class="w-full h-12 border border-gray-400 rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent" />
             
-            <input id="date" class="w-full h-full shadow-md rounded-lg border border-gray-400 focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent">
+            
             </div>
 
         </div>
 
-        <div class="flex place-content-between m-4">
-         <button class="font-semibold font-sans py-2 px-6 rounded-xl bg-gray-700 text-white hover:bg-gray-900">
+        <div class="flex m-4 place-content-between">
+         <button class="px-6 py-2 font-sans font-semibold text-white bg-gray-700 rounded-xl hover:bg-gray-900">
             Borrar
         </button>
 
-        <button class="font-semibold font-sans py-2 px-6 rounded-xl bg-gray-700 text-white hover:bg-gray-900">
+        <button class="px-6 py-2 font-sans font-semibold text-white bg-gray-700 rounded-xl hover:bg-gray-900">
             Guardar
         </button>   
         </div>
@@ -45,13 +46,23 @@
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import Datepicker from 'vue3-datepicker'
+import { ref } from 'vue'
+import { es } from 'date-fns/locale'
 
 export default {
   name: 'Add',
   components: {
-    HelloWorld
-  }
+    Datepicker
+  },
+    setup(props){
+        const picked = ref(new Date())
+        const locale = ref(es)
+        const format = ref('dd-MM-yyyy')
+        return{picked, locale, format}
+
+    }
+
 }
 </script>
